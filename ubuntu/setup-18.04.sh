57b43dab8b78a28ca7b55bff98d58843bc7e5acb
Ryan Wright <ryanwrightdev@gmail.com>
2018-08-18 18:26:34 -0400

    standard update

--- ubuntu/setup-18.04.sh
+++ ubuntu/setup-18.04.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install openssh-server -y
+sudo apt install openssh-server -y
 sudo apt install apache2 -y
-sudo apt-get install curl -y
+sudo apt install curl -y
 sudo apt install mysql-server -y
 
 sudo apt install php libapache2-mod-php php-mysql -y
 sudo apt install php-cli -y
+sudo apt install gcc make perl -y
