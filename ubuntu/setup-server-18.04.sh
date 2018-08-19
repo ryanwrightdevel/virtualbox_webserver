b93bd8fdffa9569883cfdd9e23a129fcafe2e908
Ryan Wright <ryanwrightdev@gmail.com>
2018-08-19 18:28:49 -0400

    mount changes

--- ubuntu/setup-server-18.04.sh
+++ ubuntu/setup-server-18.04.sh
@@ -10,3 +10,8 @@ sudo apt install mysql-server -y
 sudo apt install php libapache2-mod-php php-mysql -y
 sudo apt install php-cli -y
 sudo apt install gcc make perl -y
+
+#allow the ability to connect to mysql using a password.
+#set password for root as 'root'
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';"
+sudo mysql -e "FLUSH PRIVILEGES"
--- ubuntu/setup-shared-folder.sh
+++ ubuntu/setup-shared-folder.sh
@@ -2,10 +2,10 @@
 #changed this to the name of your virtualBox folder
 #NOTE: this is the name entered in the virtualBox UI, not the name or path of the folder on the hosts system
 #for example, your folder may be "c:/website" but in your virtualbox UI, you can call this anything, ex. "myWebShare"
-vboxSharedFolder=dev
+vboxSharedFolder=dev  #CHANGE THIS
 
 #please make sure this folder exists
-mountPath=/media/sf_dev #change this to the path where the shared folder should be mounted
+mountPath=/media/sf_dev #CHANGE THIS to the path where the shared folder should be mounted
 
 if [ ! -d "$mountPath" ]; then
   echo -n "The mount folder '$mountPath' does not exists. Please create it first or modify this script to to use a different path."
@@ -13,8 +13,10 @@ if [ ! -d "$mountPath" ]; then
 fi
 
 
-sudo usermod -a -G www-data vboxsf
+#add the www-data user to the vboxsf group because the shared folder is owned by vboxsf
+sudo usermod -a -G vboxsf www-data
 
 sudo mount -t vboxsf -o rw,uid=33,gid=33 $vboxSharedFolder $mountPath
 
-sudo echo "$vboxSharedFolder $mountPath   vboxsf  rw,uid=33,gid=33    0   0" >> /etc/fstab
+#add entry to automount shared folder
+echo "$vboxSharedFolder $mountPath   vboxsf  rw,uid=33,gid=33    0   0" >> sudo tee -a /etc/fstab
